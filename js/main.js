f5298b2613c92c2a7c0b2642763fedca1dfa9bdd
Enexcode <enexcode@gmail.com>
2024-12-27 13:59:24 +0600

    v1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// ========= Sponsor Slider 
+$('.sponsor_main_slider').slick({
+    // How many items you want to show
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 500,
+    arrows:false,
+  });
+
+// ========= Testimonials Slider 
+$('.reviews_main_slider').slick({
+    // How many items you want to show
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    dots: true,
+  });
